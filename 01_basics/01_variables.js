b6a8d44cf64e54a29c5bbecdf6a58bdf93558b58
Divya prakash pradhan <dp.pradhan060@gmail.com>
2025-11-09 19:15:34 +0000

    new topic

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14553
+let accountEmail = "dp.pradhan060@gmail.com"
+var accountpassword = 8018 
+accountCity = "odisha"
+// accountId = 5
+accountEmail ="gamer7735349108@gmail.com"
+accountPassword =773534
+accountCity ="delhi"
+console.log(accountEmail)
+console.log(accountPassword)
+
+console.log(accountCity)
